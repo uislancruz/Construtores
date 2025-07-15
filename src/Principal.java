cef4544b5de88ab8db97bc6bba97d1c8ee25c8ab
Uislan Cruz <uislansk8@hotmail.com>
2025-07-14 21:03:45 -0300

    Colocando o print para aparecer no console o que foi atribuido

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,6 @@
 public class Principal {
     public static void main(String[] args) {
-        Produto produto1 = new Produto("Picanha 1kg peça", 50);
+        Produto produto1 = new Produto("Picanha 1kg peça");
 
         System.out.println(produto1.nome);
         System.out.println(produto1.quantidadeEstoque);
--- src/Produto.java
+++ src/Produto.java
@@ -1,10 +1,12 @@
 public class Produto {
 
+    static final int QUANTIDADE_ESTOQUE_INICIAL = 100;
+
     String nome;
     int quantidadeEstoque;
 
-    Produto(String nome, int quantidadeEstoque){
+    Produto(String nome){
         this.nome = nome;
-        this.quantidadeEstoque = quantidadeEstoque;
+        this.quantidadeEstoque = QUANTIDADE_ESTOQUE_INICIAL;
     }
 }
