b7f9fdea3b3da2ef5553169ef7399d8c4159e44b
Uislan Cruz <uislansk8@hotmail.com>
2025-07-20 14:04:39 -0300

    Utilizando o UUID para gerar codigos aleatorios

--- src/Produto.java
+++ src/Produto.java
@@ -1,9 +1,11 @@
 import java.util.Objects;
+import java.util.UUID;
 
 public class Produto {
 
     static final int QUANTIDADE_ESTOQUE_INICIAL = 100;
 
+    final String codigo;
     String nome;
     int quantidadeEstoque;
 
@@ -24,5 +26,6 @@ public class Produto {
 
         this.nome = nome;
         this.quantidadeEstoque = estoqueInicial;
+        this.codigo = UUID.randomUUID().toString();
     }
 }
